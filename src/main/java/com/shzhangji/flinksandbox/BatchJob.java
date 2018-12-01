4cdee24bd713dd4dd2488e68f91b3237b2046b4a
Ji ZHANG <zhangji87@gmail.com>
2018-12-02 07:05:17 +0800

    streaming wordcount

--- src/main/java/com/shzhangji/flinksandbox/BatchJob.java
+++ src/main/java/com/shzhangji/flinksandbox/BatchJob.java
@@ -1,10 +1,7 @@
 package com.shzhangji.flinksandbox;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
 
 public class BatchJob {
   public static void main(String[] args) throws Exception {
@@ -14,16 +11,5 @@ public class BatchJob {
     ds.flatMap(new Tokenizer()).groupBy(0).sum(1).print();
 
     env.execute("Flink Batch Java API Skeleton");
-  }
-
-  public static class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
-    private static final long serialVersionUID = 4651156801756022364L;
-
-    @Override
-    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
-      for (String word : value.split("\\s+")) {
-        out.collect(new Tuple2<>(word, 1));
-      }
-    }
   }
 }
---
+++ src/main/java/com/shzhangji/flinksandbox/Tokenizer.java
@@ -0,0 +1,16 @@
+package com.shzhangji.flinksandbox;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+  private static final long serialVersionUID = 4651156801756022364L;
+
+  @Override
+  public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+    for (String word : value.split("\\s+")) {
+      out.collect(new Tuple2<>(word, 1));
+    }
+  }
+}
